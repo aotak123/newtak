69cc1fd972bbd354766a665cbcb5620d8e09a857
aotak123 <ao.tak@163.com>
2020-05-27 14:19:05 +0800

    5.27新增欢乐小秀才项目

---
+++ 欢乐小秀才/欢乐小秀才修改服务器时间.py
@@ -0,0 +1,51 @@
+# coding=utf-8
+import urllib2
+import time
+####################——————tak制作  vol.527——————####################
+
+
+# 定义基础url
+url = "http://test-servertime.yiqibuduoduo.com/update_time/update?f=update"#改
+
+while True:
+    type = raw_input("\n 请输入数字：\n 1：查询时间\n 2：恢复时间\n 3：修改时间\n")
+
+    if type == "1":
+        urls = "http://test-servertime.yiqibuduoduo.com/update_time/update?f=get"  # 步多多查时间
+        request = urllib2.Request(urls)
+        response = urllib2.urlopen(request)
+        print"当前服务时间："
+        print (response.read())
+        # break
+
+    if type == "2":
+        nowtime = time.strftime('%Y-%m-%d%%20%H:%M:%S')
+        fullurl2 = url + "&datetime=" + nowtime
+        # print fullurl
+        request = urllib2.Request(fullurl2)
+        response = urllib2.urlopen(request)
+        print"已恢复当前服务时间："
+        print (response.read())
+        # break
+
+    if type == "3":
+        while True:  # 无限循环语句
+            nowtime = time.strftime('%Y-%m-%d%%20%H:%M:%S')
+            print "例如：" + nowtime
+            yeartime = raw_input("请输入你想修改的时间：            ！！！请复制例子进行修改时间！！！\n恢复当前时间请输入：0\n")
+            #请使用例子中的格式，不得有误
+            if yeartime == "0":
+                nowtime = time.strftime('%Y-%m-%d%%20%H:%M:%S')
+                nowtimeurl = url + "&datetime=" + nowtime
+                # print fullurl0
+                request = urllib2.Request(nowtimeurl)
+                response = urllib2.urlopen(request)
+                print (response.read())
+                break
+
+            else:
+                fullurl3 = url + "&datetime=" + yeartime
+                # print fullurl3
+                request = urllib2.Request(fullurl3)
+                response = urllib2.urlopen(request)
+                print (response.read())
---
+++ 欢乐小秀才/欢乐小秀才清理师徒关系.py
@@ -0,0 +1,38 @@
+# coding=utf-8
+import urllib2
+####################——————tak制作  vol.527——————####################
+
+while True:
+    imei = raw_input("请输入 \033[1;31m imei\033[0m 或 \033[1;31moaid\033[0m\n")
+    if imei == "0":
+        continue
+    if imei == "":
+        continue
+    if imei == "1":
+        imei = "359596060310566" #三星
+        deviceid = "f8a37fcb1c827dce"
+
+    if imei == "2":
+        imei = "7DD773FA-74C2-455B-A893-C6A3BB8A79D3"
+        deviceid = "0019F7F2-A320-44CF-B348-88A5E10E0810"#"#iphone 7plus
+
+    if imei == "3":
+        # imei = "863056044142477"#huawei 8x
+        imei = "7ff3e7df-b96e-e8e1-baf1-f6dcf75f2be3"
+        deviceid = "94764aa49bb442e5"
+        aaid = "5a2eca205675ce4903d7f8d3881e5a6f"
+        oaid = "7ff3e7df-b96e-e8e1-baf1-f6dcf75f2be3"
+
+    if imei == "4":
+        # imei = "865973042769010"#黑鲨
+        imei = "43e219e4af34ad4e"#黑鲨
+        deviceid = "43e219e4af34ad4e"
+
+    url = "http://106.75.6.212:8020/testcleantools/cleanimei?apptypeid=100022&imei="
+    fullurl = url + imei
+
+    #print fullurl
+
+    request = urllib2.Request(fullurl)
+    response = urllib2.urlopen(request)
+    print imei + "  "+ (response.read())
